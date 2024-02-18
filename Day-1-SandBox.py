ca35a87dd98fe0a89d90e58b115f23adab5d5bf9
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 18:31:29 -0600

    Add files via upload

---
+++ Day-1-SandBox.py
@@ -0,0 +1,10 @@
+print("Hello World\nHello World\nHello World")
+print("hello " + "Clayton")
+name = input("What is your name?")
+length = len(name)
+print("Hello " + name)
+print(length)
+
+# This is my code. tinkering with print and input 
+
+# The name = input("What is your name?") is a variable
